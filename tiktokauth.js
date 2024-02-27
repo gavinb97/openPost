9a020409c67420d54a6437540e7c593cf87360d5
gavinb97 <gavinbruha@gmail.com>
2024-02-26 23:55:19 -0600

    tiktok oauth working and now can get user info

--- tiktokauth.js
+++ tiktokauth.js
@@ -22,11 +22,14 @@ const generateRandomString = (length) => {
 }
 
 const CLIENT_KEY = process.env.TIK_TOK_CLIENT_KEY // this value can be found in app's developer portal
-const SERVER_ENDPOINT_REDIRECT = 'http://localhost:3455/callback' // redirect URI should be registered in developer portal
+const SERVER_ENDPOINT_REDIRECT = 'https://moral-kindly-fly.ngrok-free.app/callback/' // redirect URI should be registered in developer portal
 const CODE_VERIFIER = generateRandomString(69)
+console.log('Code verifier ' + CODE_VERIFIER)
 const CODE_CHALLENGE = SHA256(CODE_VERIFIER).toString();
-
+console.log('Code challenge ' + CODE_CHALLENGE)
 const TIKTOK_CLIENT_SECRET = process.env.TIK_TOK_CLIENT_SECRET
+
+
 app.get('/oauth', (req, res) => {
     try {
     const csrfState = Math.random().toString(36).substring(2);
@@ -34,20 +37,16 @@ app.get('/oauth', (req, res) => {
 
     let url = `https://www.tiktok.com/v2/auth/authorize/`;
 
-    let urlParams = ''
+    // the following params need to be in `application/x-www-form-urlencoded` format.
+    url += `?client_key=${CLIENT_KEY}`;
+    url += `&scope=user.info.basic,video.publish`;
+    url += `&response_type=code`;
+    url += `&redirect_uri=${SERVER_ENDPOINT_REDIRECT}`;
+    url += `&state=` + csrfState;
+    url += `&code_challenge=${CODE_CHALLENGE}`;
+    url += `&code_challenge_method=S256`
 
-    // // the following params need to be in `application/x-www-form-urlencoded` format.
-    urlParams += `?client_key=${CLIENT_KEY}`;
-    urlParams += `&scope=user.info.basic,video.publish`;
-    urlParams += `&response_type=code`;
-    urlParams += `&redirect_uri=${SERVER_ENDPOINT_REDIRECT}`;
-    urlParams += `&state=` + csrfState;
-    urlParams += `&code_challenge=${CODE_VERIFIER}`;
-    urlParams += `&code_challenge_method=S256`
-
-    const finalURL = url + encodeURIComponent(urlParams)
-    console.log(finalURL)
-    res.redirect(finalURL);
+    res.redirect(url);
 
     } catch (err) {
         console.log(err)
@@ -58,26 +57,111 @@ app.get('/oauth', (req, res) => {
 
 app.get('/callback', async (req, res) => {
     console.log('hitting the callback ooh wee')
-    const code = req.query.code;
-    const { openId, accessToken } = await getAccessTokenAndOpenId(code, CLIENT_KEY, TIKTOK_CLIENT_SECRET);
-    console.log(openId)
-    console.log(accessToken)
+ 
+    const code = req.query.code
+  
+    const response = await getAccessTokenAndOpenId(code, CLIENT_KEY, TIKTOK_CLIENT_SECRET);
+    console.log(response)
+
+    // console.log('gonna refresh token')
+    // const refresh = await refreshAccessToken(response.refreshToken)
+    // console.log(refresh)
+
+    // console.log('gonna revoke')
+    // await revokeAccess(response.accessToken)
+
+    // await queryCreatorInfo(response.accessToken)
+
+    res.redirect('https://google.com');
 })
 
 
-const getAccessTokenAndOpenId = async (code, CLIENT_KEY, TIKTOK_CLIENT_SECRET) => {
-    let urlAccessToken = 'https://open-api.tiktok.com/oauth/access_token/';
-    urlAccessToken += '?client_key=' + CLIENT_KEY;
-    urlAccessToken += '&client_secret=' + TIKTOK_CLIENT_SECRET;
-    urlAccessToken += '&code=' + code;
-    urlAccessToken += '&grant_type=authorization_code';
-    const resp = await axios.post(urlAccessToken);
+
+
+const getAccessTokenAndOpenId = async (code) => {
+    let urlAccessToken = `https://open.tiktokapis.com/v2/oauth/token/`;
+  
+    const response = await axios.post(urlAccessToken, new URLSearchParams({
+      client_key: CLIENT_KEY,
+      client_secret: TIKTOK_CLIENT_SECRET,
+      code: code,
+      grant_type: 'authorization_code',
+      redirect_uri: SERVER_ENDPOINT_REDIRECT,
+      code_verifier: CODE_VERIFIER
+    }),
+    {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      }
+    })
+    // console.log(response)
     return {
-      accessToken: resp.data.data.access_token,
-      openId: resp.data.data.open_id,
-    };
+        accessToken: response.data.access_token,
+        openId: response.data.open_id,
+        refreshToken: response.data.refresh_token
+      };
+    
   }
 
-app.listen(3455, () => {
-    console.log('running')
-})
+  const refreshAccessToken = async (refreshToken) => {
+    let token = ''
+    if (refreshToken) {
+      token = refreshToken
+    } else {
+      console.log('no token')
+    }
+
+    let urlAccessToken = `https://open.tiktokapis.com/v2/oauth/token/`;
+  
+    const response = await axios.post(urlAccessToken, new URLSearchParams({
+      client_key: CLIENT_KEY,
+      client_secret: TIKTOK_CLIENT_SECRET,
+      grant_type: 'refresh_token',
+      refresh_token: token
+    }),
+    {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      }
+    })
+    // console.log(response)
+    return {
+        accessToken: response.data.access_token,
+        openId: response.data.open_id,
+        refreshToken: response.data.refresh_token
+      };
+  }
+
+  const revokeAccess = async (accessToken) => {
+    let urlAccessToken = `https://open.tiktokapis.com/v2/oauth/token/`;
+  
+    const response = await axios.post(urlAccessToken, new URLSearchParams({
+      client_key: CLIENT_KEY,
+      client_secret: TIKTOK_CLIENT_SECRET,
+      token: accessToken
+    }),
+    {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      }
+    })
+    console.log('revoked!!!!')
+  }
+
+  const queryCreatorInfo = async (accessToken) => {
+    let urlAccessToken = `https://open.tiktokapis.com/v2/post/publish/creator_info/query/`;
+  
+    const response = await axios.post(urlAccessToken, {},
+    {
+      headers: {
+        'Authorization': `Bearer ${accessToken}`,
+        'Content-Type': 'application/json; charset=utf-8'
+      }
+    })
+    console.log(response.data.data)
+  }
+
+queryCreatorInfo()
+// app.listen(3455, () => {
+//     console.log('running')
+// })
