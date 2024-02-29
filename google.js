88451b3d95c84dc69e38a928c82773f2c98a5599
gavinb97 <gavinbruha@gmail.com>
2024-02-28 21:48:23 -0600

    working on google oauth

--- google.js
+++ google.js
@@ -1,15 +1,20 @@
+require('dotenv').config()
 const {google} = require('googleapis');
 
 const oauth2Client = new google.auth.OAuth2(
-    YOUR_CLIENT_ID,
-    YOUR_CLIENT_SECRET,
-    YOUR_REDIRECT_URL
+    YOUTUBE_CLIENT_ID,
+    YOUTUBE_CLIENT_SECRET,
+    YOUTUBE_REDIRECT
   );
+
+  const scopes = [
+    'https://www.googleapis.com/auth/youtube.upload'
+  ];
 
   const url = oauth2Client.generateAuthUrl({
     // 'online' (default) or 'offline' (gets refresh_token)
     access_type: 'offline',
-  
-    // If you only need one scope you can pass it as a string
     scope: scopes
   });
+
+  console.log(url)
