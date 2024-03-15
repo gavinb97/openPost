fa40a88f5e2c175b9a3011d6f815d14988032e8e
gavinb97 <gavinbruha@gmail.com>
2024-03-14 23:27:13 -0500

    getting access code successfully

--- redditOauth.js
+++ redditOauth.js
@@ -10,19 +10,55 @@ const {writeTextToFile, readTokensFromFile, getVideoChunkInfo, getFileSizeInByte
 const fs = require('fs');
 app.use(cookieParser());
 app.use(cors());
+const btoa = require('btoa');
 
-
-// https://www.reddit.com/api/v1/authorize?client_id=CLIENT_ID&response_type=TYPE&
-//     state=RANDOM_STRING&redirect_uri=URI&duration=DURATION&scope=SCOPE_STRING
+const redditAccessTokenUrl = 'https://www.reddit.com/api/v1/access_token'
 const redirect_uri = 'https://moral-kindly-fly.ngrok-free.app/redditcallback/'
 const scopeSring = 'identity submit subscribe privatemessages edit mysubreddits read save'
 const stateString = generateRandomString(69)
 const loginUrl = `https://www.reddit.com/api/v1/authorize?client_id=${process.env.REDDIT_APP_ID}&response_type=code&state=${stateString}&redirect_uri=${redirect_uri}&duration=permanent&scope=${scopeSring}`
+console.log(loginUrl)
+
+
 
 app.get('/redditcallback', async (req, res) => {
     console.log('hitting the callback ooh wee')
- 
-    console.log(req)
-
+    
+    console.log(req.query)
+    const codeFromCallback = req.query.code
+    console.log(codeFromCallback)
+    const getAccessTokenResponse = await getAccessToken(codeFromCallback)
+    console.log(getAccessTokenResponse)
     res.redirect('https://google.com');
+})
+
+const getAccessToken = async (codeFromCallback) => {
+    try {
+        const authHeader = `Basic ${btoa(`${process.env.REDDIT_APP_ID}:${process.env.REDDIT_SECRET}`)}`;
+        
+        const response = await axios.post(redditAccessTokenUrl, {
+            code: codeFromCallback,
+            grant_type: 'authorization_code',
+            redirect_uri: redirect_uri,
+        }, {
+            headers: {
+                'Authorization': authHeader,
+                'Content-Type': 'application/x-www-form-urlencoded'
+            }
+        });
+
+        return response.data;
+    } catch (e) {
+        // console.log(e.response.data);
+        console.log('fucked')
+        throw e; // Re-throwing the error for handling in the caller
+    }
+}
+
+const refreshToken = async () => {
+    
+}
+
+app.listen(3455, () => {
+    console.log('running')
 })
