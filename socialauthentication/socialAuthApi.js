13a0f90d5b21efde867e68496005130b8c134087
gavinb97 <gavinbruha@gmail.com>
2024-04-03 20:02:21 -0500

    create file for service methods

--- socialauthentication/socialAuthApi.js
+++ socialauthentication/socialAuthApi.js
@@ -8,6 +8,11 @@ const { TwitterApi } = require("twitter-api-v2")
 const {writeTextToFile, readTxtFile, sleep, generateRandomString} = require('../utils')
 const axios = require('axios');
 const crypto = require('crypto');
+const {
+    generateTwitterAuthUrl,
+    getAccessToken,
+    refreshAccessToken
+} = require('./twitterService')
 
 
 const app = express();
@@ -24,81 +29,81 @@ let CodeVerifierForCall = ''
 
 
 // Twitter Stuff
-const generateTwitterAuthUrl = async () => {
-    // Define OAuth parameters
-    const oauthUrl = 'https://twitter.com/i/oauth2/authorize';
-    const responseType = 'code';
-    const scope = 'tweet.read tweet.write follows.read follows.write offline.access like.write'; // Add all required scopes here
-    const state = generateRandomString(69); // Optional: state parameter for security
-    console.log('state: ' + state)
-    const codeChallengeMethod = 'plain'; // PKCE method
-    const codeVerifier = generateRandomString(69); // Generate code verifier
-    const codeChallenge = SHA256(codeVerifier).toString();
-    CodeVerifierForCall = codeVerifier
-    console.log(CodeVerifierForCall)
-    console.log(CodeVerifierForCall.length)
-    const clientId = process.env.CLIENT_ID
+// const generateTwitterAuthUrl = async () => {
+//     // Define OAuth parameters
+//     const oauthUrl = 'https://twitter.com/i/oauth2/authorize';
+//     const responseType = 'code';
+//     const scope = 'tweet.read tweet.write follows.read follows.write offline.access like.write'; // Add all required scopes here
+//     const state = generateRandomString(69); // Optional: state parameter for security
+//     console.log('state: ' + state)
+//     const codeChallengeMethod = 'plain'; // PKCE method
+//     const codeVerifier = generateRandomString(69); // Generate code verifier
+//     const codeChallenge = SHA256(codeVerifier).toString();
+//     CodeVerifierForCall = codeVerifier
+//     console.log(CodeVerifierForCall)
+//     console.log(CodeVerifierForCall.length)
+//     const clientId = process.env.CLIENT_ID
  
-    const redirectUri = 'https://moral-kindly-fly.ngrok-free.app/xcallback'
-    // Construct the authorization URL
-    const authUrl = `${oauthUrl}?client_id=${clientId}&redirect_uri=${encodeURIComponent(redirectUri)}&response_type=${responseType}&scope=${encodeURIComponent(scope)}&state=${encodeURIComponent(state)}&code_challenge=${encodeURIComponent(CodeVerifierForCall)}&code_challenge_method=${encodeURIComponent(codeChallengeMethod)}`;
-    console.log(authUrl)
-    return authUrl;
-};
+//     const redirectUri = 'https://moral-kindly-fly.ngrok-free.app/xcallback'
+//     // Construct the authorization URL
+//     const authUrl = `${oauthUrl}?client_id=${clientId}&redirect_uri=${encodeURIComponent(redirectUri)}&response_type=${responseType}&scope=${encodeURIComponent(scope)}&state=${encodeURIComponent(state)}&code_challenge=${encodeURIComponent(CodeVerifierForCall)}&code_challenge_method=${encodeURIComponent(codeChallengeMethod)}`;
+//     console.log(authUrl)
+//     return authUrl;
+// };
 
 
-const getAccessToken = async (code) => {
+// const getAccessToken = async (code) => {
 
-    console.log(`code verifier: ${CodeVerifierForCall}`)
-    console.log(`code verifier: ${CodeVerifierForCall.length}`)
+//     console.log(`code verifier: ${CodeVerifierForCall}`)
+//     console.log(`code verifier: ${CodeVerifierForCall.length}`)
 
-    try {
-        const clientId = process.env.CLIENT_ID; // Your Twitter client ID
-        const clientSecret = process.env.CLIENT_SECRET; // Your Twitter client secret
+//     try {
+//         const clientId = process.env.CLIENT_ID; // Your Twitter client ID
+//         const clientSecret = process.env.CLIENT_SECRET; // Your Twitter client secret
        
-        const redirectUri = 'https://moral-kindly-fly.ngrok-free.app/xcallback' // Your redirect URI
+//         const redirectUri = 'https://moral-kindly-fly.ngrok-free.app/xcallback' // Your redirect URI
 
-        const response = await axios.post(
-            'https://api.twitter.com/2/oauth2/token',
-            `code=${code}&grant_type=authorization_code&client_id=${clientId}&redirect_uri=${encodeURIComponent(redirectUri)}&code_verifier=${encodeURIComponent(CodeVerifierForCall)}`,
-            {
-                headers: {
-                    'Authorization': `Basic ${Buffer.from(`${clientId}:${clientSecret}`).toString('base64')}`,
-                    'Content-Type': 'application/x-www-form-urlencoded'
-                }
-            }
-        );
+//         const response = await axios.post(
+//             'https://api.twitter.com/2/oauth2/token',
+//             `code=${code}&grant_type=authorization_code&client_id=${clientId}&redirect_uri=${encodeURIComponent(redirectUri)}&code_verifier=${encodeURIComponent(CodeVerifierForCall)}`,
+//             {
+//                 headers: {
+//                     'Authorization': `Basic ${Buffer.from(`${clientId}:${clientSecret}`).toString('base64')}`,
+//                     'Content-Type': 'application/x-www-form-urlencoded'
+//                 }
+//             }
+//         );
 
-        console.log(response.data);
-        return response.data;
-    } catch (error) {
-        console.error('Error exchanging code for access token:', error);
-        console.log(error);
-    }
-};
+//         console.log(response.data);
+//         return response.data;
+//     } catch (error) {
+//         console.error('Error exchanging code for access token:', error);
+//         console.log(error);
+//     }
+// };
 
-const getNewAccessToken = async (refreshToken) => {
-    try {
-        const clientId = process.env.CLIENT_ID;
-        const clientSecret = process.env.CLIENT_SECRET;
+// const refreshAccessToken = async (refreshToken) => {
+//     try {
+//         const clientId = process.env.CLIENT_ID;
+//         const clientSecret = process.env.CLIENT_SECRET;
 
-        const response = await axios.post(
-            'https://api.twitter.com/2/oauth2/token',
-            `refresh_token=${refreshToken}&grant_type=refresh_token&client_id=${clientId}`,
-            {
-                headers: {
-                    'Authorization': `Basic ${Buffer.from(`${clientId}:${clientSecret}`).toString('base64')}`,
-                    'Content-Type': 'application/x-www-form-urlencoded',
-                }
-            }
-        );
+//         const response = await axios.post(
+//             'https://api.twitter.com/2/oauth2/token',
+//             `refresh_token=${refreshToken}&grant_type=refresh_token&client_id=${clientId}`,
+//             {
+//                 headers: {
+//                     'Authorization': `Basic ${Buffer.from(`${clientId}:${clientSecret}`).toString('base64')}`,
+//                     'Content-Type': 'application/x-www-form-urlencoded',
+//                 }
+//             }
+//         );
 
-        console.log(response.data);
-        return response.data;
-    } catch (error) {
-        console.error('Error refreshing access token:', error);
-    }
-};
+//         console.log(response.data);
+//         return response.data;
+//     } catch (error) {
+//         console.error('Error refreshing access token:', error);
+//     }
+// };
 
 
 // twitter callback endpoint to handle successful login
@@ -112,7 +117,7 @@ app.get('/xcallback', async (req, res) => {
         console.log(tokens)
 
         console.log('getting new tokens')
-        const newTokens = await getNewAccessToken(tokens.refresh_token)
+        const newTokens = await refreshAccessToken(tokens.refresh_token)
         console.log(newTokens)
         console.log('woooot')
 
---
+++ socialauthentication/twitterService.js
@@ -0,0 +1,85 @@
+require('dotenv').config({ path: '../.env' });
+const {writeTextToFile, readTxtFile, sleep, generateRandomString} = require('../utils')
+const axios = require('axios');
+const crypto = require('crypto');
+
+
+const generateTwitterAuthUrl = async () => {
+    // Define OAuth parameters
+    const oauthUrl = 'https://twitter.com/i/oauth2/authorize';
+    const responseType = 'code';
+    const scope = 'tweet.read tweet.write follows.read follows.write offline.access like.write'; // Add all required scopes here
+    const state = generateRandomString(69); // Optional: state parameter for security
+    
+    const codeChallengeMethod = 'plain'; // PKCE method
+    const codeVerifier = generateRandomString(69); // Generate code verifier
+    // const codeChallenge = SHA256(codeVerifier).toString();
+    
+    await writeTextToFile(codeVerifier, 'codeVerifier.json')
+   
+    const clientId = process.env.CLIENT_ID
+ 
+    const redirectUri = 'https://moral-kindly-fly.ngrok-free.app/xcallback'
+    // Construct the authorization URL
+    const authUrl = `${oauthUrl}?client_id=${clientId}&redirect_uri=${encodeURIComponent(redirectUri)}&response_type=${responseType}&scope=${encodeURIComponent(scope)}&state=${encodeURIComponent(state)}&code_challenge=${encodeURIComponent(codeVerifier)}&code_challenge_method=${encodeURIComponent(codeChallengeMethod)}`;
+    console.log(authUrl)
+    return authUrl;
+};
+
+const getAccessToken = async (code) => {
+
+    const codeVerifierFromFile = await readTxtFile('codeVerifier.json')
+    console.log('from file: ' + codeVerifierFromFile)
+    try {
+        const clientId = process.env.CLIENT_ID; // Your Twitter client ID
+        const clientSecret = process.env.CLIENT_SECRET; // Your Twitter client secret
+       
+        const redirectUri = 'https://moral-kindly-fly.ngrok-free.app/xcallback' // Your redirect URI
+
+        const response = await axios.post(
+            'https://api.twitter.com/2/oauth2/token',
+            `code=${code}&grant_type=authorization_code&client_id=${clientId}&redirect_uri=${encodeURIComponent(redirectUri)}&code_verifier=${encodeURIComponent(codeVerifierFromFile)}`,
+            {
+                headers: {
+                    'Authorization': `Basic ${Buffer.from(`${clientId}:${clientSecret}`).toString('base64')}`,
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                }
+            }
+        );
+
+        console.log(response.data);
+        return response.data;
+    } catch (error) {
+        console.error('Error exchanging code for access token:', error);
+        console.log(error);
+    }
+};
+
+const refreshAccessToken = async (refreshToken) => {
+    try {
+        const clientId = process.env.CLIENT_ID;
+        const clientSecret = process.env.CLIENT_SECRET;
+
+        const response = await axios.post(
+            'https://api.twitter.com/2/oauth2/token',
+            `refresh_token=${refreshToken}&grant_type=refresh_token&client_id=${clientId}`,
+            {
+                headers: {
+                    'Authorization': `Basic ${Buffer.from(`${clientId}:${clientSecret}`).toString('base64')}`,
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                }
+            }
+        );
+
+        console.log(response.data);
+        return response.data;
+    } catch (error) {
+        console.error('Error refreshing access token:', error);
+    }
+};
+
+module.exports = {
+    generateTwitterAuthUrl,
+    getAccessToken,
+    refreshAccessToken
+}
