59bda8c738e975a0ab263d1b59ae2f7650de5f96
gavinb97 <gavinbruha@gmail.com>
2024-05-19 01:03:21 -0500

    cleanup

--- client/autopost/src/components/SetScheduleModal.js
+++ client/autopost/src/components/SetScheduleModal.js
@@ -1,16 +1,14 @@
 import React, { useState, useRef, useEffect } from 'react';
 import TagInputComponent from './TagInputComponent'; // Import the TagInputComponent
 import './../App.css';
-import {createScheduledJob} from '../service/jobService'
-import {getSFWSubreddits} from '../service/redditService'
-
+import { createScheduledJob } from '../service/jobService';
+import { getSFWSubreddits } from '../service/redditService';
 import { useAuth } from '../service/authContext';
 
 const daysOfWeek = ['S', 'M', 'T', 'W', 'Th', 'F', 'Sa'];
 
 const SetScheduleModal = ({ closeModal, selectedImages }) => {
-
-  const { user, logoutContext, loginContext  } = useAuth()
+  const { user } = useAuth();
 
   const [selectedWebsite, setSelectedWebsite] = useState('twitter'); // State for selected website
   const [picturePostOrder, setPicturePostOrder] = useState('random'); // State for picture post order
@@ -18,9 +16,7 @@ const SetScheduleModal = ({ closeModal, selectedImages }) => {
   const [scheduleInterval, setScheduleInterval] = useState(''); // State for schedule interval if schedule type is "scheduled"
   const [hourInterval, setHourInterval] = useState(1); // State for hour interval
   const [timesOfDay, setTimesOfDay] = useState([]);
-
-  const [durationOfJob, setDurationOfJob] = useState()
-
+  const [durationOfJob, setDurationOfJob] = useState();
   const [selectedDays, setSelectedDays] = useState({
     S: false,
     M: false,
@@ -28,11 +24,9 @@ const SetScheduleModal = ({ closeModal, selectedImages }) => {
     W: false,
     Th: false,
     F: false,
-    Sa: false
+    Sa: false,
   });
-
-  const [selectedSubreddits, setSelectedSubreddits] = useState();
-
+  const [selectedSubreddits, setSelectedSubreddits] = useState([]);
   const [subredditList, setSubredditList] = useState([]);
 
   useEffect(() => {
@@ -41,21 +35,13 @@ const SetScheduleModal = ({ closeModal, selectedImages }) => {
         try {
           const subreddits = await getSFWSubreddits(user);
 
-          // Create an array of objects with a unique ID for each subreddit
           const subredditObjects = subreddits.map((subredditName, index) => ({
             name: subredditName,
             id: `${index + 1}`, // ID using numerical values
           }));
 
           setSubredditList(subredditObjects);
-
-          // Initialize selectedSubreddits with all subreddits from subredditList
-          setSelectedSubreddits(
-            subredditObjects.map((sub) => ({
-              id: sub.id,
-              name: sub.name,
-            }))
-          );
+          setSelectedSubreddits(subredditObjects);
         } catch (error) {
           console.error('Error fetching subreddits:', error);
         }
@@ -63,52 +49,39 @@ const SetScheduleModal = ({ closeModal, selectedImages }) => {
 
       fetchSubreddits();
     } else {
-      // Clear subredditList and selectedSubreddits when not 'reddit'
       setSubredditList([]);
       setSelectedSubreddits([]);
     }
   }, [selectedWebsite, user]);
 
   const [isOpen, setIsOpen] = useState(false);
-    const dropdownRef = useRef(null);
+  const dropdownRef = useRef(null);
 
-    // Toggle the visibility of the dropdown
-    const toggleDropdown = () => setIsOpen(!isOpen);
+  const toggleDropdown = () => setIsOpen(!isOpen);
 
-    useEffect(() => {
-      // Initialize selectedSubreddits with all subreddits from subredditList
-      console.log('subredditList:', subredditList);
-      setSelectedSubreddits(
-        subredditList.map(sub => ({
-          id: sub.id,
-          name: sub.name,
-        }))
-      );
-    }, [subredditList]);
+  useEffect(() => {
+    setSelectedSubreddits(subredditList);
+  }, [subredditList]);
 
-    const handleCheckboxChange = (event) => {
-      const { value } = event.target;
-      const subreddit = subredditList.find(sub => sub.id === value);
-    
-      setSelectedSubreddits((prev) => {
-        // Check if the subreddit is already in the selectedSubreddits array
-        const exists = prev.some(sub => sub.id === value);
-    
-        if (exists) {
-          // If it exists, remove it
-          return prev.filter(sub => sub.id !== value);
-        } else {
-          // If it doesn't exist, add the subreddit
-          return [...prev, { id: subreddit.id, name: subreddit.name }];
-        }
-      });
-    };
+  const handleCheckboxChange = (event) => {
+    const { value } = event.target;
+    const subreddit = subredditList.find((sub) => sub.id === value);
 
+    setSelectedSubreddits((prev) => {
+      const exists = prev.some((sub) => sub.id === value);
+
+      if (exists) {
+        return prev.filter((sub) => sub.id !== value);
+      } else {
+        return [...prev, { id: subreddit.id, name: subreddit.name }];
+      }
+    });
+  };
 
   const handleDayClick = (day) => {
-    setSelectedDays(prevState => ({
+    setSelectedDays((prevState) => ({
       ...prevState,
-      [day]: !prevState[day]
+      [day]: !prevState[day],
     }));
   };
 
@@ -135,7 +108,6 @@ const SetScheduleModal = ({ closeModal, selectedImages }) => {
   const handleAddTime = () => {
     setTimesOfDay([...timesOfDay, { hour: '1', minute: '00', ampm: 'am' }]);
   };
-  
 
   const handleTimeChange = (index, field, value) => {
     const updatedTimes = timesOfDay.map((time, i) => {
@@ -146,7 +118,6 @@ const SetScheduleModal = ({ closeModal, selectedImages }) => {
     });
     setTimesOfDay(updatedTimes);
   };
-  
 
   const handleRemoveTime = (index) => {
     const updatedTimesOfDay = [...timesOfDay];
@@ -154,14 +125,12 @@ const SetScheduleModal = ({ closeModal, selectedImages }) => {
     setTimesOfDay(updatedTimesOfDay);
   };
 
-  
   const handleDurationChange = (e) => {
     setDurationOfJob(e.target.value);
   };
 
   const handleSave = async () => {
-    const username = user.username
-    // Create JSON object with all the state data
+    const username = user.username;
     const scheduleData = {
       username,
       selectedWebsite,
@@ -173,20 +142,13 @@ const SetScheduleModal = ({ closeModal, selectedImages }) => {
       selectedDays,
       selectedImages,
       durationOfJob,
-      selectedSubreddits
+      selectedSubreddits,
     };
- 
-    // Log the JSON object
+
     console.log('Schedule Data:', scheduleData);
-    await createScheduledJob(scheduleData)
-    // Close modal
-    // closeModal();
+    await createScheduledJob(scheduleData);
   };
 
-    if(selectedSubreddits) {
-      console.log(selectedSubreddits)
-    }
-    console.log(selectedImages)
   return (
     <div className="SetScheduleModal-modal-container">
       <div className="SetScheduleModal-modal-backdrop" onClick={closeModal}></div>
@@ -194,11 +156,7 @@ const SetScheduleModal = ({ closeModal, selectedImages }) => {
         <h2>Set Posting Schedule</h2>
         <div className="input-group">
           <label htmlFor="website">Website:</label>
-          <select
-            id="website"
-            value={selectedWebsite}
-            onChange={handleWebsiteChange}
-          >
+          <select id="website" value={selectedWebsite} onChange={handleWebsiteChange}>
             <option value="twitter">Twitter</option>
             <option value="reddit">Reddit</option>
             <option value="tiktok">TikTok</option>
@@ -208,11 +166,7 @@ const SetScheduleModal = ({ closeModal, selectedImages }) => {
 
         <div className="input-group">
           <label htmlFor="postOrderSelect">Picture post order:</label>
-          <select
-            id="postOrderSelect"
-            value={picturePostOrder}
-            onChange={handlePicturePostOrderChange}
-          >
+          <select id="postOrderSelect" value={picturePostOrder} onChange={handlePicturePostOrderChange}>
             <option value="random">Random</option>
             <option value="order">In order</option>
           </select>
@@ -220,11 +174,7 @@ const SetScheduleModal = ({ closeModal, selectedImages }) => {
 
         <div className="input-group">
           <label htmlFor="scheduleTypeSelect">Schedule Type:</label>
-          <select
-            id="scheduleTypeSelect"
-            value={scheduleType}
-            onChange={handleScheduleTypeChange}
-          >
+          <select id="scheduleTypeSelect" value={scheduleType} onChange={handleScheduleTypeChange}>
             <option value="random">Random</option>
             <option value="scheduled">Scheduled</option>
           </select>
@@ -233,11 +183,7 @@ const SetScheduleModal = ({ closeModal, selectedImages }) => {
         {scheduleType === 'random' && (
           <div className="input-group">
             <label htmlFor="durationSelect">Job duration: </label>
-            <select
-              id="durationSelect"
-              value={durationOfJob}
-              onChange={handleDurationChange}
-            >
+            <select id="durationSelect" value={durationOfJob} onChange={handleDurationChange}>
               <option value="">Select duration</option>
               <option value="forever">Forever</option>
               <option value="1">1 iteration</option>
@@ -246,19 +192,14 @@ const SetScheduleModal = ({ closeModal, selectedImages }) => {
               <option value="4">4 iterations</option>
               <option value="5">5 iterations</option>
             </select>
-            <p>*a single interation is every photo selected posted a singular time</p>
+            <p>*a single iteration is every photo selected posted a singular time</p>
           </div>
-        
         )}
 
         {scheduleType === 'scheduled' && (
           <div className="input-group">
             <label htmlFor="scheduleIntervalSelect">Schedule Interval:</label>
-            <select
-              id="scheduleIntervalSelect"
-              value={scheduleInterval}
-              onChange={handleScheduleIntervalChange}
-            >
+            <select id="scheduleIntervalSelect" value={scheduleInterval} onChange={handleScheduleIntervalChange}>
               <option value="">Select interval</option>
               <option value="hour">Hour Intervals</option>
               <option value="set">Set Times</option>
@@ -269,12 +210,11 @@ const SetScheduleModal = ({ closeModal, selectedImages }) => {
         {scheduleType === 'scheduled' && scheduleInterval === 'hour' && (
           <div className="input-group">
             <label>Every:</label>
-            <select
-              value={hourInterval}
-              onChange={handleHourIntervalChange}
-            >
+            <select value={hourInterval} onChange={handleHourIntervalChange}>
               {[...Array(24)].map((_, index) => (
-                <option key={index} value={index + 1}>{index + 1}</option>
+                <option key={index} value={index + 1}>
+                  {index + 1}
+                </option>
               ))}
             </select>
             <span>hours</span>
@@ -282,42 +222,33 @@ const SetScheduleModal = ({ closeModal, selectedImages }) => {
         )}
 
         {scheduleType === 'scheduled' && scheduleInterval === 'set' && (
-        <div className="input-group">
-        {timesOfDay.map((time, index) => (
-          <div key={index} style={{ display: 'flex', alignItems: 'center' }}>
-            <select
-              value={time.hour}
-              onChange={(e) => handleTimeChange(index, 'hour', e.target.value)}
-            >
-              {[...Array(12)].map((_, i) => (
-                <option key={i} value={i + 1}>
-                  {i + 1}
-                </option>
-              ))}
-            </select>
-            <select
-              value={time.minute}
-              onChange={(e) => handleTimeChange(index, 'minute', e.target.value)}
-            >
-              {[...Array(60)].map((_, i) => (
-                <option key={i} value={i}>
-                  {i < 10 ? `0${i}` : i}
-                </option>
-              ))}
-            </select>
-            <select
-              value={time.ampm}
-              onChange={(e) => handleTimeChange(index, 'ampm', e.target.value)}
-            >
-              <option value="am">AM</option>
-              <option value="pm">PM</option>
-            </select>
-            <button onClick={() => handleRemoveTime(index)}>x</button> {/* "x" button appears for every input */}
+          <div className="input-group">
+            {timesOfDay.map((time, index) => (
+              <div key={index} style={{ display: 'flex', alignItems: 'center' }}>
+                <select value={time.hour} onChange={(e) => handleTimeChange(index, 'hour', e.target.value)}>
+                  {[...Array(12)].map((_, i) => (
+                    <option key={i} value={i + 1}>
+                      {i + 1}
+                    </option>
+                  ))}
+                </select>
+                <select value={time.minute} onChange={(e) => handleTimeChange(index, 'minute', e.target.value)}>
+                  {[...Array(60)].map((_, i) => (
+                    <option key={i} value={i}>
+                      {i < 10 ? `0${i}` : i}
+                    </option>
+                  ))}
+                </select>
+                <select value={time.ampm} onChange={(e) => handleTimeChange(index, 'ampm', e.target.value)}>
+                  <option value="am">AM</option>
+                  <option value="pm">PM</option>
+                </select>
+                <button onClick={() => handleRemoveTime(index)}>x</button>
+              </div>
+            ))}
+            <button onClick={handleAddTime}>Add Time</button>
           </div>
-        ))}
-        <button onClick={handleAddTime}>Add Time</button>
-      </div>
-      )}
+        )}
 
         {scheduleType === 'scheduled' && scheduleInterval === 'set' && (
           <div className="input-group">
@@ -333,41 +264,39 @@ const SetScheduleModal = ({ closeModal, selectedImages }) => {
           </div>
         )}
 
-      {(selectedWebsite === 'twitter' || selectedWebsite === 'youtube' || selectedWebsite === 'tiktok') && (
-          <div >
+        {(selectedWebsite === 'twitter' || selectedWebsite === 'youtube' || selectedWebsite === 'tiktok') && (
+          <div>
             <label>Mandatory hashtags (Optional): </label>
-            <input></input>
+            <input />
           </div>
         )}
 
-{selectedWebsite === 'reddit' && subredditList.length > 0 && (
-      <div className="your-component">
-        <div className="subreddit-selector" ref={dropdownRef}>
-          <button onClick={toggleDropdown}>
-              Select Subreddits
-          </button>
-          {isOpen && (
-            <div className="dropdown-menu">
-              <div className="grid-container">
-                {subredditList.map(subreddit => (
-                  <div key={subreddit.id} className="grid-item">
-                    <label>
-                      <input
-                        type="checkbox"
-                        value={subreddit.id}
-                        checked={selectedSubreddits.some(sub => sub.id === subreddit.id)}
-                        onChange={handleCheckboxChange}
-                      />
-                      <span>{subreddit.name}</span>
-                    </label>
+        {selectedWebsite === 'reddit' && subredditList.length > 0 && (
+          <div className="your-component">
+            <div className="subreddit-selector" ref={dropdownRef}>
+              <button onClick={toggleDropdown}>Select Subreddits</button>
+              {isOpen && (
+                <div className="dropdown-menu">
+                  <div className="grid-container">
+                    {subredditList.map((subreddit) => (
+                      <div key={subreddit.id} className="grid-item">
+                        <label>
+                          <input
+                            type="checkbox"
+                            value={subreddit.id}
+                            checked={selectedSubreddits.some((sub) => sub.id === subreddit.id)}
+                            onChange={handleCheckboxChange}
+                          />
+                          <span>{subreddit.name}</span>
+                        </label>
+                      </div>
+                    ))}
                   </div>
-                ))}
-              </div>
+                </div>
+              )}
             </div>
-          )}
-        </div>
-      </div>
-)}
+          </div>
+        )}
 
         <button onClick={handleSave}>Save</button>
         <button onClick={closeModal}>Close</button>
--- jobs/jobService.js
+++ jobs/jobService.js
@@ -5,16 +5,20 @@ const formatRequest = async (request) => {
     console.log('request ^^')
     switch (request.selectedWebsite) {
         case 'reddit':
-            console.log('reddit')
+            console.log('reddit is selected')
+            await handleScheduleType(request)
             break;
         case 'twitter': 
-            console.log('twitter')
+            console.log('twitter is selected')
+            await handleScheduleType(request)
             break;
         case 'tiktok':
-            console.log('tiktok')
+            console.log('tiktok is selected')
+            await handleScheduleType(request)
             break;
         case 'youtube':
-            console.log('youtube')
+            console.log('youtube is selected')
+            await handleScheduleType(request)
             break;
         default:
             console.log('No website selected, something must be wrong')
@@ -22,7 +26,68 @@ const formatRequest = async (request) => {
     
 }
 
+const handleScheduleType = async (request) => {
+    switch (request.scheduleType) {
+        case 'random':
+            console.log('random schedule chosen')
+            await handleRandomIntervalDuration(request)
+            break;
+        case 'scheduled':
+            console.log('scheduled chosen')
+            break
+        default:
+            console.log('No schedule selected, something might be wrong')
+    }
+}
 
+const handleRandomIntervalDuration = async (request) => {
+    switch (request) {
+        case 'forever': 
+            console.log('Job will run forever')
+            break;
+        case '1':
+            console.log('Job will run for 1 iteration')
+            break;
+        case '2':
+            console.log('Job will run for 2 iteration')
+            break;
+        case '3':
+            console.log('Job will run for 3 iteration')
+            break;
+        case '4': 
+            console.log('Job will run for 4 iteration')
+            break;
+        case '5':
+            console.log('Job will run for 5 iteration')
+            break;
+        default:
+            console.log('No duration selected')
+    }
+
+}
+
+const handleScheduleIntervals = async (request) => {
+    switch (request.scheduleInterval) {
+        case 'hour': 
+            console.log('hour interval selected')
+            break;
+        case 'set':
+            console.log('set interval selected')
+            break;
+        default:
+            console.log('No schedule interval selected, something is wrong')
+    }
+}
+
+const handleHourInterval = async (request) => {
+    if (request.hourInterval) {
+        console.log(`job will run every ${request.hourInterval}`)
+    }
+}
+
+const handleSetInterval = async (request) => {
+    
+}
 
 module.exports = {
     formatRequest
