e06b1e1d0ac1493ee19ad68310290b737772061c
gavinb97 <gavinbruha@gmail.com>
2024-08-21 20:00:09 -0500

    start authorize accounts, update tos

---
+++ client/autopost/src/components/AuthorizeAccounts.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../App.css';
+
+const AuthorizeAccounts = () => {
+
+
+    return (
+        <div>
+            <p>hi</p>
+        </div>
+    )
+}
+
+export default AuthorizeAccounts
