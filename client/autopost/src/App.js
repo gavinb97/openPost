0792297421d654a9dc9a07a09e7b47bcdb2e4e27
Gavin <gavin@Gavins-Air.lan>
2024-10-14 00:06:28 -0500

    add adsense?

--- client/autopost/src/App.js
+++ client/autopost/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import UploadScreen from './screens/UploadScreen';
 import SocialsLoginScreen from './screens/SocialsLoginScreen';
@@ -19,6 +19,16 @@ import Payment from './screens/Payment';
 import ProSuccessScreen from './screens/ProSuccessScreen';
 
 function App () {
+
+  useEffect(() => {
+    const script = document.createElement('script')
+    script.src = 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1024690452826457'
+    script.async = true
+    script.crossOrigin = 'anonymous'
+    document.body.appendChild(script)
+    console.log('created ad script')
+  }, [])
+
   const router = createBrowserRouter([
     {
       path: '/',
