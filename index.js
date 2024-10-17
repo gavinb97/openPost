acdb738f0e19ffb50b9398c63144d7cd23500b75
Gavin <gavin@Gavins-Air.lan>
2024-10-16 20:18:08 -0500

    add certs and cors

--- index.js
+++ index.js
@@ -1,26 +1,37 @@
-const express = require('express');
-const app = express();
-const cors = require('cors');
+const express = require('express') 
+const https = require('https') 
+const fs = require('fs') 
+const cors = require('cors') 
 
-const stripeRoute = require('./socialauthentication/stripeRoute');
-const openPostMediaApi = require('./openPostMediaApi');
-const jobsApi = require('./../openPost/jobs/jobsApi');
-const socialAuthApi = require('./socialauthentication/socialAuthApi');
+const stripeRoute = require('./socialauthentication/stripeRoute') 
+const openPostMediaApi = require('./openPostMediaApi') 
+const jobsApi = require('./../openPost/jobs/jobsApi') 
+const socialAuthApi = require('./socialauthentication/socialAuthApi') 
 
+const app = express() 
 
+const corsOptions = {
+  origin: ['https://only-posts.com/', 'https://onlypostsai.com/', ],
+  optionsSuccessStatus: 200,
+};
 
-app.use(cors());
+app.use(cors(corsOptions)) 
 
-app.use(stripeRoute);
-app.use(openPostMediaApi);
-app.use(jobsApi);
-app.use(socialAuthApi);
+app.use(stripeRoute) 
+app.use(openPostMediaApi) 
+app.use(jobsApi) 
+app.use(socialAuthApi) 
 
-
+// Load SSL certificate and key
+const sslOptions = {
+  key: fs.readFileSync('./certs/only-posts.com.key'),
+  cert: fs.readFileSync('./certs/only-posts.com.pem')
+} 
 
 // 3455
-const PORT = process.env.PORT || 3455;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
- 
+const PORT = process.env.PORT || 3455 
+
+// Start HTTPS server
+https.createServer(sslOptions, app).listen(PORT, () => {
+  console.log(`HTTPS Server is running on port ${PORT}`) 
+}) 
