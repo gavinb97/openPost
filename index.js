30f8f78144800b48aa24894e85338aee246a71ec
Gavin <gavin@Gavins-Air.lan>
2024-10-16 23:05:07 -0500

    serve over https

--- index.js
+++ index.js
@@ -23,19 +23,19 @@ app.use(jobsApi)
 app.use(socialAuthApi) 
 
 // Load SSL certificate and key
-// const sslOptions = {
-//   key: fs.readFileSync('./certs/only-posts.com.key'),
-//   cert: fs.readFileSync('./certs/only-posts.com.pem')
-// } 
+const sslOptions = {
+  key: fs.readFileSync('./certs/only-posts.com.key'),
+  cert: fs.readFileSync('./certs/only-posts.com.pem')
+} 
 
 // 3455
 const PORT = process.env.PORT || 3455 
 
 // Start HTTPS server
-// https.createServer(sslOptions, app).listen(PORT, () => {
-//   console.log(`HTTPS Server is running on port ${PORT}`) 
-// }) 
+https.createServer(sslOptions, app).listen(PORT, () => {
+  console.log(`HTTPS Server is running on port ${PORT}`) 
+}) 
 
-app.listen(PORT, () => {
-  console.log('HTTP server is running at PORT 3455')
-})
+// app.listen(PORT, () => {
+//   console.log('HTTP server is running at PORT 3455')
+// })
